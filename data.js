fe6b249b3d409aa5d7062de7db3ecabf98a25fd8
NHUNG <nhungnt.c3ks@nghean.edu.vn>
2026-03-06 15:01:55 +0700

    Update data.js

--- data.js
+++ data.js
@@ -6,8 +6,8 @@ const dataHocLieu = {
         link_ai_tro_ly: "https://www.coze.com/s/Za8nHrfvF/"
     },
     bai_hoc: [
-        { id: 1, tieu_de: "BÀI 2: THIẾT BỊ THÔNG MINH", mo_ta: "PHÂN LOẠI THIẾT BỊ.", link: "baihoc/bai2_k10/index.html", icon: "📱", phan_loai: "Lớp 10" },
-        { id: 2, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "BÀI HỌC PYTHON.", link: "https://python-bai-17.vercel.app/", icon: "🐍", phan_loai: "Lớp 10" },
+        { id: 1, tieu_de: "BÀI 2: THIẾT BỊ THÔNG MINH", mo_ta: "trò chơi kéo - thả phân loại thiết bị thông minh.", link: "baihoc/bai2_k10/index.html", icon: "📱", phan_loai: "Lớp 10" },
+        { id: 2, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "Hoàn thành bài học để mở link azota.", link: "https://baif21-python.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
         { id: 3, tieu_de: "BÀI 21: VÒNG LẶP WHILE", mo_ta: "BÀI HỌC PYTHON.", link: "baihoc/python-while/index.html", icon: "🐍", phan_loai: "Lớp 10" },
         { id: 4, tieu_de: "BÀI 5: KẾT NỐI THIẾT BỊ SỐ", mo_ta: "MÁY TÍNH VỚI XÃ HỘI TRI THỨC", link: "#", icon: "💻", phan_loai: "Lớp 11" },
         { id: 5, tieu_de: "Binary Search", mo_ta: "CHUYÊN ĐỀ HSG", link: "baihoc/hsg1/index.html", icon: "🏆", phan_loai: "HSG" },
@@ -17,9 +17,8 @@ const dataHocLieu = {
         { id: 9, tieu_de: "SÁCH GIÁO KHOA TIN 11", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📖", phan_loai: "Lớp 11" },
         { id: 10, tieu_de: "SÁCH GIÁO KHOA TIN 12", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📖", phan_loai: "Lớp 12" },
         { id: 11, tieu_de: "CÂU LỆNH LẶP FOR", mo_ta: "BÀI HỌC PYTHON.", link: "https://hoclieuso-navy.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
-        { id: 12, tieu_de: "BÀI 21: CÂU LỆNH LẶP WHILE", mo_ta: "BÀI HỌC PYTHON.", link: "https://baif21-python.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
-        { id: 13, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "BÀI HỌC PYTHON.", link: "baihoc/bai17_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" },
-        { id: 14, tieu_de: "M21: ÔN TẬP WHILE", mo_ta: "KIỂM TRA ĐÁNH GIÁ.", link: "baihoc/bai21_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" }
+        { id: 12, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "BÀI HỌC PYTHON.", link: "baihoc/bai17_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" },
+        { id: 13, tieu_de: "ÔN TẬP WHILE", mo_ta: "KIỂM TRA ĐÁNH GIÁ.", link: "baihoc/bai21_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" }
     ],
     prompts_ai: {
         tro_choi: [{ id: 1, tieu_de: "TẠO TRÒ CHƠI Ô CHỮ", icon: "🧩", noi_dung_prompt: "Hãy đóng vai chuyên gia giáo dục tạo bộ câu hỏi ô chữ..." }],
@@ -27,5 +26,6 @@ const dataHocLieu = {
     },
     danh_sach_video: [{ id: 1, tieu_de: "BÀI GIẢNG PYTHON", link: "#", icon: "🎬" }]
 };
+
 
 
