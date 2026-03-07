c861729378b198dfae4a3f09edcdca536db834fc
NHUNG <nhungnt.c3ks@nghean.edu.vn>
2026-03-07 21:57:02 +0700

    Update data.js

--- data.js
+++ data.js
@@ -6,19 +6,19 @@ const dataHocLieu = {
         link_ai_tro_ly: "https://www.coze.com/s/Za8nHrfvF/"
     },
     bai_hoc: [
-        { id: 1, tieu_de: "BÀI 2: THIẾT BỊ THÔNG MINH", mo_ta: "trò chơi kéo - thả phân loại thiết bị thông minh.", link: "baihoc/bai2_k10/index.html", icon: "📱", phan_loai: "Lớp 10" },
-        { id: 2, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "Hoàn thành bài học để mở link azota.", link: "https://baif21-python.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
-        { id: 3, tieu_de: "BÀI 21: VÒNG LẶP WHILE", mo_ta: "BÀI HỌC PYTHON.", link: "baihoc/python-while/index.html", icon: "🐍", phan_loai: "Lớp 10" },
-        { id: 4, tieu_de: "BÀI 5: KẾT NỐI THIẾT BỊ SỐ", mo_ta: "MÁY TÍNH VỚI XÃ HỘI TRI THỨC", link: "#", icon: "💻", phan_loai: "Lớp 11" },
-        { id: 5, tieu_de: "Binary Search", mo_ta: "CHUYÊN ĐỀ HSG", link: "baihoc/hsg1/index.html", icon: "🏆", phan_loai: "HSG" },
-        { id: 6, tieu_de: "Lùa bò vào chuồng", mo_ta: "CHUYÊN ĐỀ HSG", link: "baihoc/hsg2/index.html", icon: "🧠", phan_loai: "HSG" },
-        { id: 7, tieu_de: "Sliding Windows", mo_ta: "CHUYÊN ĐỀ HSG", link: "baihoc/hsg3/index.html", icon: "🏆", phan_loai: "HSG" },
-        { id: 8, tieu_de: "SÁCH GIÁO KHOA TIN 10", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📚", phan_loai: "Lớp 10" },
-        { id: 9, tieu_de: "SÁCH GIÁO KHOA TIN 11", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📖", phan_loai: "Lớp 11" },
-        { id: 10, tieu_de: "SÁCH GIÁO KHOA TIN 12", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📖", phan_loai: "Lớp 12" },
-        { id: 11, tieu_de: "CÂU LỆNH LẶP FOR", mo_ta: "BÀI HỌC PYTHON.", link: "https://hoclieuso-navy.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
-        { id: 12, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "BÀI HỌC PYTHON.", link: "baihoc/bai17_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" },
-        { id: 13, tieu_de: "ÔN TẬP WHILE", mo_ta: "KIỂM TRA ĐÁNH GIÁ.", link: "baihoc/bai21_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" }
+        { id: 1, tieu_de: "BÀI 2: THIẾT BỊ THÔNG MINH", mo_ta: "củng cố kiến thức qua trò chơi", link: "baihoc/bai2_k10/index.html", icon: "📱", phan_loai: "Lớp 10" },
+        { id: 2, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "Hoàn thành bài học để mở link Azota.", link: "https://hoclieuso-navy.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
+	    { id: 3, tieu_de: "BÀI 19: CÂU LỆNH RẺ NHÁNH", mo_ta: "Bài giảng.", link: "baihoc/bai19_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" },
+        { id: 4, tieu_de: "BÀI 21: VÒNG LẶP WHILE", mo_ta: "BÀI HỌC PYTHON.", link: "baihoc/python-while/index.html", icon: "🐍", phan_loai: "Lớp 10" },
+        { id: 5, tieu_de: "BÀI 5: KẾT NỐI THIẾT BỊ SỐ", mo_ta: "MÁY TÍNH VỚI XÃ HỘI TRI THỨC", link: "#", icon: "💻", phan_loai: "Lớp 11" },
+        { id: 6, tieu_de: "Binary Search", mo_ta: "thuật toán mô phỏng", link: "baihoc/hsg1/index.html", icon: "🏆", phan_loai: "HSG" },
+        { id: 7, tieu_de: "Lùa bò vào chuồng", mo_ta: "thuật toán mô phỏng", link: "baihoc/hsg2/index.html", icon: "🧠", phan_loai: "HSG" },
+        { id: 8, tieu_de: "Sliding Windows", mo_ta: "thuật toán mô phỏng", link: "baihoc/hsg3/index.html", icon: "🏆", phan_loai: "HSG" },
+        { id: 9, tieu_de: "ÔN TẬP VÒNG LẶP WHILE", mo_ta: "KIỂM TRA ĐÁNH GIÁ THƯỜNG XUYÊN", link: "baihoc/bai21_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" },
+        { id: 10, tieu_de: "SÁCH GIÁO KHOA TIN 10", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📚", phan_loai: "Lớp 10" },
+        { id: 11, tieu_de: "SÁCH GIÁO KHOA TIN 11", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📖", phan_loai: "Lớp 11" },
+        { id: 12, tieu_de: "SÁCH GIÁO KHOA TIN 12", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📖", phan_loai: "Lớp 12" }
+      
     ],
     prompts_ai: {
         tro_choi: [{ id: 1, tieu_de: "TẠO TRÒ CHƠI Ô CHỮ", icon: "🧩", noi_dung_prompt: "Hãy đóng vai chuyên gia giáo dục tạo bộ câu hỏi ô chữ..." }],
