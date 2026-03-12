ee3e1242e80e1e184ea35366f4880b3928c09dcf
NHUNG <nhungnt.c3ks@nghean.edu.vn>
2026-03-13 00:20:52 +0700

    Update data.js

--- data.js
+++ data.js
@@ -8,14 +8,14 @@ const dataHocLieu = {
     bai_hoc: [
         { id: 1, tieu_de: "BÀI 2: THIẾT BỊ THÔNG MINH", mo_ta: "củng cố kiến thức qua trò chơi", link: "baihoc/bai2_k10/index.html", icon: "📱", phan_loai: "Lớp 10" },
         { id: 2, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "Hoàn thành bài học để mở link Azota.", link: "https://hoclieuso-navy.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
-	    { id: 3, tieu_de: "BÀI 18: CÂU LỆNH VÀO RA ĐƠN GIẢN", mo_ta: "Bài giảng.", link: "https://bai18-python.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
-	    { id: 4, tieu_de: "BÀI 19: CÂU LỆNH RẺ NHÁNH", mo_ta: "Bài giảng.", link: "baihoc/bai19_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" },
-        { id: 5, tieu_de: "BÀI 21: VÒNG LẶP WHILE", mo_ta: "BÀI KIỂM TRA, ĐÁNH GIÁ.", link: "baihoc/python-while/index.html", icon: "🐍", phan_loai: "Lớp 10" },
+	{ id: 3, tieu_de: "BÀI 18: CÂU LỆNH VÀO RA ĐƠN GIẢN", mo_ta: "Bài giảng.", link: "https://bai18-python.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
+	{ id: 4, tieu_de: "BÀI 19: CÂU LỆNH RẼ NHÁNH", mo_ta: "Bài giảng.", link: "baihoc/bai19_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" },
+        { id: 5, tieu_de: "ÔN TẬP VÒNG LẶP WHILE", mo_ta: "BÀI KIỂM TRA, ĐÁNH GIÁ.", link: "baihoc/python-while/index.html", icon: "🐍", phan_loai: "Lớp 10" },
         { id: 6, tieu_de: "BÀI 5: KẾT NỐI THIẾT BỊ SỐ", mo_ta: "MÁY TÍNH VỚI XÃ HỘI TRI THỨC", link: "#", icon: "💻", phan_loai: "Lớp 11" },
         { id: 7, tieu_de: "Binary Search", mo_ta: "thuật toán mô phỏng", link: "baihoc/hsg1/index.html", icon: "🏆", phan_loai: "HSG" },
         { id: 8, tieu_de: "Lùa bò vào chuồng", mo_ta: "thuật toán mô phỏng", link: "baihoc/hsg2/index.html", icon: "🧠", phan_loai: "HSG" },
         { id: 9, tieu_de: "Sliding Windows", mo_ta: "thuật toán mô phỏng", link: "baihoc/hsg3/index.html", icon: "🏆", phan_loai: "HSG" },
-        { id: 10, tieu_de: "ÔN TẬP VÒNG LẶP WHILE", mo_ta: "KIỂM TRA ĐÁNH GIÁ THƯỜNG XUYÊN", link: "baihoc/bai21_k10/index.html", icon: "🐍", phan_loai: "Lớp 10" },
+        { id: 10, tieu_de: "kiểm tra kiến thức vòng lặp FOR", mo_ta: "KIỂM TRA ĐÁNH GIÁ THƯỜNG XUYÊN", link: "https://kiemtradanhgia.vercel.app", icon: "🐍", phan_loai: "Lớp 10" },
         { id: 11, tieu_de: "SÁCH GIÁO KHOA TIN 10", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📚", phan_loai: "Lớp 10" },
         { id: 12, tieu_de: "SÁCH GIÁO KHOA TIN 11", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📖", phan_loai: "Lớp 11" },
         { id: 13, tieu_de: "SÁCH GIÁO KHOA TIN 12", mo_ta: "FILE PDF CHUẨN", link: "https://sachgiaokhoa.online/", icon: "📖", phan_loai: "Lớp 12" }
