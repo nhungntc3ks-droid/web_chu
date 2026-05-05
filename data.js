625720950b94e12dd6731a250f22bd4c1e4e7117
NHUNG <nhungnt.c3ks@nghean.edu.vn>
2026-05-05 15:37:46 +0700

    Update data.js

--- data.js
+++ data.js
@@ -6,12 +6,12 @@ const dataHocLieu = {
         link_ai_tro_ly: "https://www.coze.com/s/Za8nHrfvF/"
     },
     bai_hoc: [
-    { id: 1, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "TỰ HỌC - LUYỆN TẬP - KIỂM TRA", link: "https://bai17-linkchung.vercel.app", icon: "📖", phan_loai: "Lớp 10" },
-	{ id: 2, tieu_de: "BÀI 18: CÁC LỆNH VÀO RA ĐƠN GIẢN", mo_ta: "TỰ HỌC - LUYỆN TẬP - KIỂM TRA", link: "https://bai18-python.vercel.app", icon: "📖", phan_loai: "Lớp 10" },
-	{ id: 3, tieu_de: "BÀI 19: CÂU LẸNH RẼ NHÁNH IF", mo_ta: "TỰ HỌC - LUYỆN TẬP - KIỂM TRA ", link: "https://bai19-python.vercel.app", icon: "📖", phan_loai: "Lớp 10" },
-	{ id: 4, tieu_de: "BÀI 20: CÂU LẸNH lẶP FOR", mo_ta: " TỰ HỌC - LUYỆN TẬP - KIỂM TRA", link: "https://bai20-python.vercel.app", icon: "•	✅", phan_loai: "Lớp 10" },
+    { id: 1, tieu_de: "BÀI 17: BIẾN VÀ LỆNH GÁN", mo_ta: "TỰ HỌC - LUYỆN TẬP - KIỂM TRA", link: "https://bai17-linkchung.vercel.app", icon: "📚", phan_loai: "Lớp 10" },
+	{ id: 2, tieu_de: "BÀI 18: CÁC LỆNH VÀO RA ĐƠN GIẢN", mo_ta: "TỰ HỌC - LUYỆN TẬP - KIỂM TRA", link: "https://bai18-python.vercel.app", icon: "🎓", phan_loai: "Lớp 10" },
+	{ id: 3, tieu_de: "BÀI 19: CÂU LẸNH RẼ NHÁNH IF", mo_ta: "TỰ HỌC - LUYỆN TẬP - KIỂM TRA ", link: "https://bai19-python.vercel.app", icon: "💻", phan_loai: "Lớp 10" },
+	{ id: 4, tieu_de: "BÀI 20: CÂU LẸNH lẶP FOR", mo_ta: " TỰ HỌC - LUYỆN TẬP - KIỂM TRA", link: "https://bai20-python.vercel.app", icon: "•	🎨", phan_loai: "Lớp 10" },
 	{ id: 5, tieu_de: "BÀI 21: CÂU LẸNH lẶP WHILE", mo_ta: " TỰ HỌC - LUYỆN TẬP - KIỂM TRA", link: "https://bai21-python.vercel.app", icon: "•	✅", phan_loai: "Lớp 10" },
-	{ id: 6, tieu_de: "BÀI 22: KIỂU DỮ LIỆU DANH SÁCH", mo_ta: " TỰ HỌC - LUYỆN TẬP - KIỂM TRA", link: "https://bai22-python.vercel.app", icon: "•	✅", phan_loai: "Lớp 10" },
+	{ id: 6, tieu_de: "BÀI 22: KIỂU DỮ LIỆU DANH SÁCH", mo_ta: " TỰ HỌC - LUYỆN TẬP - KIỂM TRA", link: "https://bai22-python.vercel.app", icon: "•	✍️", phan_loai: "Lớp 10" },
       ],
     prompts_ai: {
         tro_choi: [{ id: 1, tieu_de: "TẠO TRÒ CHƠI Ô CHỮ", icon: "🧩", noi_dung_prompt: "Hãy đóng vai chuyên gia giáo dục tạo bộ câu hỏi ô chữ..." }],
